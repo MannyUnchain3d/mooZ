9fc835162f0ded9528c03970f06b47022a5d309c
Manny <manny@unchain3d.dev>
2024-04-21 01:54:59 +0700

    MeetingSetup done

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,5 +1,40 @@
-const Meeting = ({ params }: { params: { id: string } }) => {
-  return <div>Meeting Room: #{params.id}</div>;
+"use client";
+
+import { useState } from "react";
+import { useUser } from "@clerk/nextjs";
+import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+import MeetingSetup from "@/components/MeetingSetup";
+import MeetingRoom from "@/components/MeetingRoom";
+import { useGetCallById } from "@/hooks/useGetCallById";
+import Loader from "@/components/Loader";
+
+const MeetingPage = ({ params: { id } }: { params: { id: string } }) => {
+  const { user, isLoaded } = useUser();
+  const { call, isCallLoading } = useGetCallById(id);
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+
+  if (!isLoaded || isCallLoading) return <Loader />;
+
+  if (!call)
+    return (
+      <p className="text-center text-3xl font-bold text-white">
+        Call Not Found
+      </p>
+    );
+
+  return (
+    <main className="h-screen w-full">
+      <StreamCall call={call}>
+        <StreamTheme>
+          {!isSetupComplete ? (
+            <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
+          ) : (
+            <MeetingRoom />
+          )}
+        </StreamTheme>
+      </StreamCall>
+    </main>
+  );
 };
 
-export default Meeting;
+export default MeetingPage;
